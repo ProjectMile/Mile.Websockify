314e9c8f79cc0a37df3e3d969fc98ca6c6779018
Naruto Mouri <Mouri_Naruto@outlook.com>
2020-09-10 01:39:58 +0800

    Add implementations for the example.

--- Mile.Websockify.Example/Startup.cs
+++ Mile.Websockify.Example/Startup.cs
@@ -19,23 +19,25 @@ namespace Mile.Websockify.Example
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add 
+        // services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure
+        // the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
+
+            app.UseWebsockify("/websockify", "192.168.50.120", 5900);
+
+            app.UseDefaultFiles();
 
             app.UseStaticFiles();
 
